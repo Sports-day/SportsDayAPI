b53ad0379637597fcff094be746ce0ccc770dec8
testusuke <silverbro007@icloud.com>
2023-01-16 00:11:34 +0900

    initial commit

---
+++ src/main/kotlin/dev/t7e/Application.kt
@@ -0,0 +1,19 @@
+package dev.t7e
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import dev.t7e.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureSecurity()
+    configureRouting()
+}
