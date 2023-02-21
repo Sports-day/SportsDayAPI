56ee9ab9a4f493e4deefd7a0ef18bf347b49d50f
testusuke <silverbro007@icloud.com>
2023-02-21 22:13:17 +0900

    feat: configuration

--- src/main/kotlin/dev/t7e/Application.kt
+++ src/main/kotlin/dev/t7e/Application.kt
@@ -1,14 +1,10 @@
 package dev.t7e
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import dev.t7e.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     configureHTTP()
