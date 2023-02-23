9c8a1537bd4714bb093d75a97b8554e4d2660f04
testusuke <silverbro007@icloud.com>
2023-02-23 11:42:17 +0900

    change: versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val exposed_version = "0.41.1"
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.0"
