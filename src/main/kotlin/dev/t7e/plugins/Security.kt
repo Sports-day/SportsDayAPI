d00613339603a8234c1810b837145df771d8503b
testusuke <silverbro007@icloud.com>
2023-02-28 09:39:28 +0900

    feat: role-based authorization

--- src/main/kotlin/dev/t7e/plugins/Security.kt
+++ src/main/kotlin/dev/t7e/plugins/Security.kt
@@ -4,22 +4,37 @@ import com.auth0.jwk.GuavaCachedJwkProvider
 import com.auth0.jwk.UrlJwkProvider
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import dev.t7e.models.AdminUser
+import dev.t7e.models.MicrosoftAccount
 import dev.t7e.utils.Email
 import io.ktor.server.auth.*
 import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.lang.Exception
 import java.net.URL
 import java.security.interfaces.RSAPublicKey
+import java.time.LocalDateTime
 
 //  Key Provider
-val azureADKeyURI = URL("https://login.microsoftonline.com/${System.getenv("AZURE_AD_TENANT_ID") ?: "common"}/discovery/keys")
+val azureADKeyURI =
+    URL("https://login.microsoftonline.com/${System.getenv("AZURE_AD_TENANT_ID") ?: "common"}/discovery/keys")
+
+//  cache
+val cache = mutableMapOf<String, UserPrincipal?>()
 
 fun Application.configureSecurity() {
 
     authentication {
         bearer("azure-ad") {
             realm = "Access to the / route"
-            authenticate {bearerCredential ->
+            authenticate { bearerCredential ->
+                //  cache
+                if (cache.containsKey(bearerCredential.token)) {
+                    println("Auth: use cache.")
+
+                    return@authenticate cache[bearerCredential.token]
+                }
+
                 val jwt = JWT.decode(bearerCredential.token)
                 val keyId = jwt.keyId
                 //  cached jwk provider
@@ -42,11 +57,39 @@ fun Application.configureSecurity() {
                     //  check if allowed domain
                     if (!email.isAllowedDomain()) return@authenticate null
 
-                    println("allowed")
+                    //  get microsoft user (or create user)
+                    val microsoftAccount = if (!MicrosoftAccount.existMicrosoftAccount(email.toString())) {
+                        //  create
+                        transaction {
+                            MicrosoftAccount.new {
+                                this.email = email.toString()
+                                this.name = jwt.claims["name"]?.asString() ?: "Unknown"
+                                this.mailAccountName = email.username()
+                                this.firstLogin = LocalDateTime.now()
+                                this.lastLogin = LocalDateTime.now()
+                            }
+                        }
+                    } else {
+                        //  get
+                        MicrosoftAccount.getMicrosoftAccount(email.toString())
+                    }
 
-                    null
+                    //  if not exist
+                    if (microsoftAccount == null) {
+                        return@authenticate null
+                    }
+
+                    println("Authorized: ${microsoftAccount.name}")
+
+                    //  cache
+                    cache[bearerCredential.token] = UserPrincipal(
+                        microsoftAccount,
+                        if (AdminUser.isAdminUserByEmail(email.toString())) setOf(Role.ADMIN) else setOf(Role.USER)
+                    )
+                    cache[bearerCredential.token]
                 } catch (e: Exception) {
-                    println("Failed to validate token")
+                    println("Failed to validate token.")
+                    e.printStackTrace()
 
                     return@authenticate null
                 }
@@ -54,3 +97,14 @@ fun Application.configureSecurity() {
         }
     }
 }
+
+
+enum class Role(val value: String) {
+    ADMIN("ADMIN"),
+    USER("USER")
+}
+
+data class UserPrincipal(
+    val microsoftAccount: MicrosoftAccount,
+    val roles: Set<Role> = emptySet()
+) : Principal
