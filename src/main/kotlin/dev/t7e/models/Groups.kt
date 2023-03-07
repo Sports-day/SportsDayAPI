87e9abc60ae8e4a7aac619118a5bfa34f445b808
testusuke <silverbro007@icloud.com>
2023-03-07 23:13:35 +0900

    change: using memorizeOneObject

--- src/main/kotlin/dev/t7e/models/Groups.kt
+++ src/main/kotlin/dev/t7e/models/Groups.kt
@@ -1,11 +1,14 @@
 package dev.t7e.models
 
+import dev.t7e.utils.Cache
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.javatime.datetime
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.time.Duration.Companion.minutes
 
 /**
  * Created by testusuke on 2023/02/25
@@ -18,7 +21,19 @@ object Groups : IntIdTable("groups") {
 }
 
 class GroupEntity(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<GroupEntity>(Groups)
+    companion object : IntEntityClass<GroupEntity>(Groups) {
+        val getAllGroups = Cache.memoizeOneObject(1.minutes) {
+            transaction {
+                GroupEntity.all().toList()
+            }
+        }
+
+        val getGroup: (id: Int) -> GroupEntity? = Cache.memoize(1.minutes) { id ->
+            transaction {
+                GroupEntity.findById(id)
+            }
+        }
+    }
 
     var name by Groups.name
     var description by Groups.description
