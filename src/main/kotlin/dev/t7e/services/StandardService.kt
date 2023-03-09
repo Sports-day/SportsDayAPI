9266466e2092acd56ff75428b31207aa130b0018
testusuke <silverbro007@icloud.com>
2023-03-09 18:44:49 +0900

    docs: standard service documentation

--- src/main/kotlin/dev/t7e/services/StandardService.kt
+++ src/main/kotlin/dev/t7e/services/StandardService.kt
@@ -8,6 +8,17 @@ import org.jetbrains.exposed.sql.transactions.transaction
  * Created by testusuke on 2023/03/07
  * @author testusuke
  */
+
+/**
+ * This extendable class include id-based R(read)D(delete) feature
+ *
+ * @param T the id-based table
+ * @param R the serializable model
+ * @property objectName the name of this object
+ * @property _getAllObjectFunction the lambda of get all object
+ * @property _getObjectByIdFunction the lambda of get object by id
+ * @property _serialize the serializer
+ */
 open class StandardService<T : IntEntity, R>(
     private val objectName: String,
     private val _getAllObjectFunction: () -> List<T>,
