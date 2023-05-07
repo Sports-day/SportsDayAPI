2eecaca078f461987131d266a43ae92994eaae00
testusuke <silverbro007@icloud.com>
2023-05-08 00:00:54 +0900

    feat: Enum utility function

---
+++ src/main/kotlin/dev/t7e/utils/EnumUtil.kt
@@ -0,0 +1,15 @@
+package dev.t7e.utils
+
+/**
+ * Created by testusuke on 2023/05/07
+ * @author testusuke
+ */
+
+
+inline fun <reified T : Enum<T>> safeValueOf(type: String): T? {
+    return try {
+        java.lang.Enum.valueOf(T::class.java, type)
+    } catch (e: IllegalArgumentException) {
+        null
+    }
+}
