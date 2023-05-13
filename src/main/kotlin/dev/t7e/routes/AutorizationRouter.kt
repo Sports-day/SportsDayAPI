06543043004c69d22c54737c5aee70b05ec6cc2d
testusuke <silverbro007@icloud.com>
2023-05-14 00:22:44 +0900

    change: return ms id

--- src/main/kotlin/dev/t7e/routes/AutorizationRouter.kt
+++ src/main/kotlin/dev/t7e/routes/AutorizationRouter.kt
@@ -1,9 +1,12 @@
 package dev.t7e.routes
 
+import dev.t7e.plugins.UserPrincipal
+import dev.t7e.utils.DataMessageResponse
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
 
 /**
  * Created by testusuke on 2023/02/20
@@ -15,8 +18,18 @@ fun Route.authorizationRouting() {
     authenticate {
         route("authorization") {
             get {
-                call.respondText("Authorization route")
+                call.respond(
+                    DataMessageResponse(
+                        "authorized",
+                        Me(call.authentication.principal<UserPrincipal>()?.microsoftAccountId ?: -1)
+                    )
+                )
             }
         }
     }
 }
+
+@Serializable
+data class Me(
+    val microsoftAccountId: Int
+)
