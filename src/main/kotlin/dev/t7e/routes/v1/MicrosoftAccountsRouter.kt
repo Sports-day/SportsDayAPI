2437b5025dc96993b91b514b6920ead0e06432e6
testusuke <silverbro007@icloud.com>
2023-05-17 15:47:56 +0900

    fix: permission

--- src/main/kotlin/dev/t7e/routes/v1/MicrosoftAccountsRouter.kt
+++ src/main/kotlin/dev/t7e/routes/v1/MicrosoftAccountsRouter.kt
@@ -26,16 +26,18 @@ import kotlinx.serialization.Serializable
 
 fun Route.microsoftAccountsRouter() {
     route("/microsoft-accounts") {
-        withRole(Role.ADMIN) {
-            /**
-             * Get all microsoft accounts
-             */
-            get {
-                val accounts = MicrosoftAccountsService.getAll()
+        withRole(Role.USER) {
+            withRole(Role.ADMIN) {
+                /**
+                 * Get all microsoft accounts
+                 */
+                get {
+                    val accounts = MicrosoftAccountsService.getAll()
 
-                call.respond(HttpStatusCode.OK, DataResponse(accounts.getOrDefault(listOf())))
+                    call.respond(HttpStatusCode.OK, DataResponse(accounts.getOrDefault(listOf())))
+                }
+
             }
-
             route("/{id?}") {
 
                 /**
@@ -43,91 +45,101 @@ fun Route.microsoftAccountsRouter() {
                  */
                 get {
                     val id = getId(call)
+
+                    val ms = call.authentication.principal<UserPrincipal>()
+                        ?: throw BadRequestException("failed to get user principal")
+
+                    if (!ms.roles.contains(Role.ADMIN) && ms.microsoftAccount.id.value != id) {
+                        throw BadRequestException("you dont have permission to link this account")
+                    }
+
                     MicrosoftAccountsService.getById(id)
                         .respondOrInternalError {
                             call.respond(HttpStatusCode.OK, DataResponse(it))
                         }
                 }
 
-                /**
-                 * Delete specific microsoft account
-                 */
-                delete {
-                    val id = getId(call)
-                    MicrosoftAccountsService.deleteById(id)
-                        .respondOrInternalError {
-                            call.respond(HttpStatusCode.OK, MessageResponse("deleted microsoft account"))
-                            //  Logger
-                            Logger.commit(
-                                "[MicrosoftAccountsRouter] deleted microsoft account: $id",
-                                LogEvents.DELETE,
-                                call.authentication.principal<UserPrincipal>()?.microsoftAccount
-                            )
-                        }
-                }
-
-                route("/role") {
+                withRole(Role.ADMIN) {
                     /**
-                     * Update specific microsoft account role
+                     * Delete specific microsoft account
                      */
-                    put {
+                    delete {
                         val id = getId(call)
-                        val role = call.receive<AccountRoleRequest>()
-
-                        MicrosoftAccountsService
-                            .setAccountRole(id, role.role)
+                        MicrosoftAccountsService.deleteById(id)
                             .respondOrInternalError {
-                                call.respond(
-                                    HttpStatusCode.OK,
-                                    DataMessageResponse(
-                                        "updated account role",
-                                        it
-                                    )
+                                call.respond(HttpStatusCode.OK, MessageResponse("deleted microsoft account"))
+                                //  Logger
+                                Logger.commit(
+                                    "[MicrosoftAccountsRouter] deleted microsoft account: $id",
+                                    LogEvents.DELETE,
+                                    call.authentication.principal<UserPrincipal>()?.microsoftAccount
                                 )
                             }
                     }
-                }
 
-                withRole(Role.USER) {
-                    route("/link-user") {
+                    route("/role") {
+                        /**
+                         * Update specific microsoft account role
+                         */
                         put {
                             val id = getId(call)
-                            val requestBody = call.receive<LinkUserRequest>()
-
-                            val ms = call.authentication.principal<UserPrincipal>()
-                                ?: throw BadRequestException("failed to get user principal")
-
-                            if (!ms.roles.contains(Role.ADMIN) && ms.microsoftAccount.id.value != id) {
-                                throw BadRequestException("you dont have permission to link this account")
-                            }
+                            val role = call.receive<AccountRoleRequest>()
 
                             MicrosoftAccountsService
-                                .linkUser(id, requestBody.userId)
+                                .setAccountRole(id, role.role)
                                 .respondOrInternalError {
-                                    call.respond(HttpStatusCode.OK, MessageResponse("link user"))
-                                }
-                        }
-
-                        delete {
-                            val id = getId(call)
-                            val ms = call.authentication.principal<UserPrincipal>()
-                                ?: throw BadRequestException("failed to get user principal")
-
-                            if (!ms.roles.contains(Role.ADMIN) && ms.microsoftAccount.id.value != id) {
-                                throw BadRequestException("you dont have permission to link this account")
-                            }
-
-                            MicrosoftAccountsService
-                                .unlinkUser(id)
-                                .respondOrInternalError {
-                                    call.respond(HttpStatusCode.OK, MessageResponse("unlink user"))
+                                    call.respond(
+                                        HttpStatusCode.OK,
+                                        DataMessageResponse(
+                                            "updated account role",
+                                            it
+                                        )
+                                    )
                                 }
                         }
                     }
                 }
+
+                route("/link-user") {
+                    put {
+                        val id = getId(call)
+                        val requestBody = call.receive<LinkUserRequest>()
+
+                        val ms = call.authentication.principal<UserPrincipal>()
+                            ?: throw BadRequestException("failed to get user principal")
+
+                        if (!ms.roles.contains(Role.ADMIN) && ms.microsoftAccount.id.value != id) {
+                            throw BadRequestException("you dont have permission to link this account")
+                        }
+
+                        MicrosoftAccountsService
+                            .linkUser(id, requestBody.userId)
+                            .respondOrInternalError {
+                                call.respond(HttpStatusCode.OK, MessageResponse("link user"))
+                            }
+                    }
+
+                    delete {
+                        val id = getId(call)
+                        val ms = call.authentication.principal<UserPrincipal>()
+                            ?: throw BadRequestException("failed to get user principal")
+
+                        if (!ms.roles.contains(Role.ADMIN) && ms.microsoftAccount.id.value != id) {
+                            throw BadRequestException("you dont have permission to link this account")
+                        }
+
+                        MicrosoftAccountsService
+                            .unlinkUser(id)
+                            .respondOrInternalError {
+                                call.respond(HttpStatusCode.OK, MessageResponse("unlink user"))
+                            }
+                    }
+                }
+
             }
         }
     }
+
 }
 
 /**
