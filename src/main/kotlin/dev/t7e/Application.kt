0fd833f1f6989944f1a163f70c2cc8af015e8f1c
testusuke <silverbro007@icloud.com>
2023-05-21 22:42:02 +0900

    fix: batch off

--- src/main/kotlin/dev/t7e/Application.kt
+++ src/main/kotlin/dev/t7e/Application.kt
@@ -23,7 +23,7 @@ fun Application.module() {
     //  DB
     DatabaseManager
     //  tables
-    initializeTables(true)
+    initializeTables()
     //  configuration
     KeyValueStore
     //  Log
