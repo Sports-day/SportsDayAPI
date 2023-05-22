9ef57fab5c51caa416448c31e4aef8f43b69f873
testusuke <silverbro007@icloud.com>
2023-05-22 17:03:48 +0900

    fix: disable rate limit

--- src/main/kotlin/dev/t7e/Application.kt
+++ src/main/kotlin/dev/t7e/Application.kt
@@ -17,7 +17,7 @@ fun Application.module() {
     configureSerialization()
     configureSecurity()
     configureRouting()
-    configureRateLimit()
+//    configureRateLimit()
     configureStatusPage()
 
     //  DB
