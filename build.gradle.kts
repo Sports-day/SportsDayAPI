a90979f1ea595c51b09f1d85d6f777ef9fdef4d5
testusuke <testusuke@gmail.com>
2024-03-25 12:08:41 +0900

    feat: jwt authentication

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-rate-limit:$ktor_version")
     implementation("io.ktor:ktor-server-status-pages:$ktor_version")
--- src/main/kotlin/net/sportsday/plugins/Security.kt
+++ src/main/kotlin/net/sportsday/plugins/Security.kt
@@ -1,15 +1,44 @@
 package net.sportsday.plugins
 
+import io.ktor.http.*
+import io.ktor.http.auth.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
+import net.sportsday.utils.JwtConfig
+import net.sportsday.utils.MessageResponse
 
 fun Application.configureSecurity() {
-//    authentication {
-//        bearer {
-//            realm = "Access to the / route"
-//            authenticate { bearerCredential ->
-//                //  auth
-//                Authorization.authorize(bearerCredential)
-//            }
-//        }
-//    }
+    authentication {
+        jwt {
+            realm = System.getenv("JWT_REALMS") ?: throw IllegalArgumentException("No JWT realms found")
+
+            verifier(JwtConfig.verifier)
+
+            authHeader { call ->
+                //  TODO change YOUR_COOKIE_NAME_FOR_TOKEN
+                val cookieValue = call.request.cookies["YOUR_COOKIE_NAME_FOR_TOKEN"] ?: return@authHeader null
+                println("cookieValue: $cookieValue")
+
+                try {
+                    parseAuthorizationHeader("Bearer $cookieValue")
+                } catch (cause: IllegalArgumentException) {
+                    cause.message
+                    null
+                }
+            }
+
+            validate { credential ->
+                credential.payload.getClaim("id").asString()?.let { id ->
+                    //  TODO find user by id
+                    JWTPrincipal(credential.payload)
+                }
+            }
+
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, MessageResponse("Token is not valid or has expired"))
+            }
+        }
+    }
 }
