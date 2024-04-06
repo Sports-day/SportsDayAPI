842d24e4234ff8243045eeaca57d9cbeef7ffd0f
testusuke <testusuke@gmail.com>
2024-04-06 11:44:23 +0900

    feat: add permission router

---
+++ src/main/kotlin/net/sportsday/routes/v1/PermissionsRouter.kt
@@ -0,0 +1,29 @@
+package net.sportsday.routes.v1
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import net.sportsday.services.PermissionsService
+import net.sportsday.utils.DataResponse
+import net.sportsday.utils.respondOrInternalError
+
+/**
+ * Created by testusuke on 2024/04/06
+ * @author testusuke
+ */
+
+fun Route.permissionsRoute() {
+    route("/permissions") {
+        get {
+            PermissionsService
+                .getAll()
+                .respondOrInternalError {
+                    call.respond(
+                        HttpStatusCode.OK,
+                        DataResponse(it)
+                    )
+                }
+        }
+    }
+}
