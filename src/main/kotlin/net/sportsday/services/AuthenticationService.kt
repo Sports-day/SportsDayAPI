22f9b0408ea34f8b3b867976791b4a90b9544173
testusuke <testusuke@gmail.com>
2024-04-21 20:42:27 +0900

    refactor: remove unused import

--- src/main/kotlin/net/sportsday/services/AuthenticationService.kt
+++ src/main/kotlin/net/sportsday/services/AuthenticationService.kt
@@ -12,7 +12,6 @@ import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.plugins.*
 import io.ktor.util.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
