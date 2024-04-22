3076783780753393de74c852f9deb4552b603701
testusuke <testusuke@gmail.com>
2024-04-22 13:37:56 +0900

    fix: Serializableをつけ忘れていたためクラッシュする問題

--- src/main/kotlin/net/sportsday/models/Permission.kt
+++ src/main/kotlin/net/sportsday/models/Permission.kt
@@ -23,59 +23,71 @@ sealed class Permission(
     val name: String,
     val description: String
 ) {
+    @Serializable
     object AccessPolicy : Permission(
         name = "AccessPolicy",
         description = "アクセスポリシー"
     ) {
+        @Serializable
         object Read : Permission(
             name = "AccessPolicy.Read",
             description = "アクセスポリシーの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "AccessPolicy.Write",
             description = "アクセスポリシーの書き込み"
         )
     }
 
+    @Serializable
     object Class : Permission(
         name = "Class",
         description = "クラス"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Class.Read",
             description = "クラスの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Class.Write",
             description = "クラスの書き込み"
         )
     }
 
+    @Serializable
     object User : Permission(
         name = "User",
         description = "ユーザー"
     ) {
+        @Serializable
         object Read : Permission(
             name = "User.Read",
             description = "ユーザーの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "User.Write",
             description = "ユーザーの書き込み"
         )
 
+        @Serializable
         object Role : Permission(
             name = "User.Role",
             description = "ユーザーのロール"
         ) {
+            @Serializable
             object Read : Permission(
                 name = "User.Role.Read",
                 description = "ユーザーのロールの読み取り"
             )
 
+            @Serializable
             object Write : Permission(
                 name = "User.Role.Write",
                 description = "ユーザーのロールの書き込み"
@@ -83,175 +95,210 @@ sealed class Permission(
         }
     }
 
+    @Serializable
     object Team : Permission(
         name = "Team",
         description = "チーム"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Team.Read",
             description = "チームの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Team.Write",
             description = "チームの書き込み"
         )
     }
 
+    @Serializable
     object TeamTag : Permission(
         name = "TeamTag",
         description = "チームタグ"
     ) {
+        @Serializable
         object Read : Permission(
             name = "TeamTag.Read",
             description = "チームタグの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "TeamTag.Write",
             description = "チームタグの書き込み"
         )
     }
 
+    @Serializable
     object Tag : Permission(
         name = "Tag",
         description = "タグ"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Tag.Read",
             description = "タグの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Tag.Write",
             description = "タグの書き込み"
         )
     }
 
+    @Serializable
     object Sport : Permission(
         name = "Sport",
         description = "スポーツ"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Sport.Read",
             description = "スポーツの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Sport.Write",
             description = "スポーツの書き込み"
         )
     }
 
+    @Serializable
     object Game : Permission(
         name = "Game",
         description = "ゲーム"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Game.Read",
             description = "ゲームの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Game.Write",
             description = "ゲームの書き込み"
         )
     }
 
+    @Serializable
     object Match : Permission(
         name = "Match",
         description = "マッチ"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Match.Read",
             description = "マッチの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Match.Write",
             description = "マッチの書き込み"
         )
     }
 
+    @Serializable
     object Location : Permission(
         name = "Location",
         description = "ロケーション"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Location.Read",
             description = "ロケーションの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Location.Write",
             description = "ロケーションの書き込み"
         )
     }
 
+    @Serializable
     object Information : Permission(
         name = "Information",
         description = "情報"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Information.Read",
             description = "情報の読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Information.Write",
             description = "情報の書き込み"
         )
     }
 
+    @Serializable
     object Image : Permission(
         name = "Image",
         description = "画像"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Image.Read",
             description = "画像の読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Image.Write",
             description = "画像の書き込み"
         )
     }
 
+    @Serializable
     object PermissionManager : Permission(
         name = "Permission",
         description = "権限管理"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Permission.Read",
             description = "権限管理の読み取り"
         )
     }
 
+    @Serializable
     object Role : Permission(
         name = "Role",
         description = "ロール"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Role.Read",
             description = "ロールの読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Role.Write",
             description = "ロールの書き込み"
         )
     }
 
+    @Serializable
     object Configuration : Permission(
         name = "Configuration",
         description = "設定"
     ) {
+        @Serializable
         object Read : Permission(
             name = "Configuration.Read",
             description = "設定の読み取り"
         )
 
+        @Serializable
         object Write : Permission(
             name = "Configuration.Write",
             description = "設定の書き込み"
