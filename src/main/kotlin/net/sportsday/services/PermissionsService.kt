6edc94ca2dd0fe050f4bbf8fd5f163893a7a01e1
testusuke <testusuke@gmail.com>
2024-04-23 00:47:03 +0900

    fix: 正常にシリアライズできない問題を修正

--- src/main/kotlin/net/sportsday/services/PermissionsService.kt
+++ src/main/kotlin/net/sportsday/services/PermissionsService.kt
@@ -1,15 +1,16 @@
 package net.sportsday.services
 
-import net.sportsday.models.Permission
+import net.sportsday.models.Permission.Companion.serialize
 import net.sportsday.models.PermissionList
+import net.sportsday.models.SerializablePermission
 
 /**
  * Created by testusuke on 2024/04/06
  * @author testusuke
  */
 object PermissionsService {
-    fun getAll(): Result<List<Permission>> {
-        val permissions = PermissionList.getAll()
+    fun getAll(): Result<List<SerializablePermission>> {
+        val permissions = PermissionList.getAll().map { it.serialize() }
 
         return Result.success(
             permissions
